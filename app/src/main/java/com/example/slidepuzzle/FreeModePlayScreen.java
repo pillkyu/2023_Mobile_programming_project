6f5149b87ab58c9cd77cfa5faa4209ffd58ef696
BossTeemo <ab3433@naver.com>
2023-11-23 07:29:12 +0900

    자유모드플레이화면구현,자유모드선택화면에서 난이도설정버튼 라디오버튼으로수정

--- app/src/main/java/com/example/slidepuzzle/FreeModePlayScreen.java
+++ app/src/main/java/com/example/slidepuzzle/FreeModePlayScreen.java
@@ -1,4 +1,15 @@
 package com.example.slidepuzzle;
 
-public class FreeModePlayScreen {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FreeModePlayScreen extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_mode_play_screen);
+
+    }
+
 }
--- app/src/main/java/com/example/slidepuzzle/FreeModeScreen.java
+++ app/src/main/java/com/example/slidepuzzle/FreeModeScreen.java
@@ -1,5 +1,6 @@
 package com.example.slidepuzzle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -7,10 +8,21 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FreeModeScreen extends AppCompatActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_mode_screen);
+
+        //btn_gamestart 구현
+        ImageButton btnFreeMode = findViewById(R.id.btn_game_start);
+        btnFreeMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FreeModeScreen.this, FreeModePlayScreen.class);
+                startActivity(intent);
+            }
+        });
 
         //btn_back 구현
         ImageButton btnBack = findViewById(R.id.btn_back);
