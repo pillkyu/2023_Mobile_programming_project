83e926ab63b105b71741accb67384f589a7f8c09
BossTeemo <ab3433@naver.com>
2023-11-22 18:23:46 +0900

    홈화면 버튼구현 및 도전모드구현

---
+++ app/src/main/java/com/example/slidepuzzle/ChallengeModeScreen.java
@@ -0,0 +1,13 @@
+package com.example.slidepuzzle;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChallengeModeScreen extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_mode_screen);
+    }
+}
--- app/src/main/java/com/example/slidepuzzle/StartScreen.java
+++ app/src/main/java/com/example/slidepuzzle/StartScreen.java
@@ -3,12 +3,23 @@ package com.example.slidepuzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 public class StartScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        ImageButton btnChallengeMode = findViewById(R.id.btn_challengeMode);
+        btnChallengeMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreen.this, ChallengeModeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
