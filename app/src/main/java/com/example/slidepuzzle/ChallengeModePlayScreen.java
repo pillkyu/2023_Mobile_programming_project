c81f567e5aaea3544950371acd5dd89abe2d6c7c
BossTeemo <ab3433@naver.com>
2023-11-24 01:51:02 +0900

    free mode play screen작성, hint버튼,home버튼 구현

--- app/src/main/java/com/example/slidepuzzle/ChallengeModePlayScreen.java
+++ app/src/main/java/com/example/slidepuzzle/ChallengeModePlayScreen.java
@@ -1,4 +1,49 @@
 package com.example.slidepuzzle;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ImageButton;
 
-public class ChallengeModePlayScreen {
+public class ChallengeModePlayScreen extends AppCompatActivity {
+    public int hintCount = 3; // 초기 힌트 개수
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_mode_play_screen);
+
+        ImageButton hintButton = findViewById(R.id.btn_hint_counted);
+
+        hintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hintCount > 0) {
+                    hintCount--; // 힌트 사용
+                    updateHintButtonImage(hintButton, hintCount);
+                } else {
+                    // 힌트를 모두 사용한 상태 (0/3)
+                    // 추가적인 동작이 필요하면 여기에 구현
+                }
+            }
+        });
+
+        // 초기 이미지 설정
+        updateHintButtonImage(hintButton, hintCount);
+    }
+
+    public void updateHintButtonImage(ImageButton hintButton, int count) {
+        switch (count) {
+            case 3:
+                hintButton.setImageResource(R.drawable.btn_hint_3_selector);
+                break;
+            case 2:
+                hintButton.setImageResource(R.drawable.btn_hint_2_selector);
+                break;
+            case 1:
+                hintButton.setImageResource(R.drawable.btn_hint_1_selector);
+                break;
+            case 0:
+                hintButton.setImageResource(R.drawable.btn_hint_0_selector);
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/slidepuzzle/FreeModePlayScreen.java
+++ app/src/main/java/com/example/slidepuzzle/FreeModePlayScreen.java
@@ -1,8 +1,12 @@
 package com.example.slidepuzzle;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 public class FreeModePlayScreen extends AppCompatActivity {
     @Override
@@ -10,6 +14,32 @@ public class FreeModePlayScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_mode_play_screen);
 
-    }
+        ImageButton btnHint = findViewById(R.id.btn_hint);
+        FragmentContainerView fragmentContainerView = findViewById(R.id.fragment_container);
 
+        //hint버튼 눌렷을 때의 동작 구현
+        btnHint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Fragment표시
+                fragmentContainerView.setVisibility(View.VISIBLE);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new HintFragment())
+                        .commit();
+            }
+        });
+
+        //홈버튼 구현
+        ImageButton btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //BtnHome클래스의 goHome 메서드 호출
+                BtnHome.goHome(FreeModePlayScreen.this);
+            }
+        });
+
+    }
 }
+
+
